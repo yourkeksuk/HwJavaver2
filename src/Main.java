e09c1448b0ecfc859b9dfea86526ca5dbf869b6d
yourkeksuk <igor-18css@mail.ru>
2022-07-28 14:43:17 +0300

    Finish commit ver2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
 
-        int sell = 2000;      // Стоимость билета
-        int bonus = 20;        // Количество рублей для одной бонусной милли
-
-        int miles = sell / bonus;     // Количество начисляемых бонусных милль
-
-        System.out.println((sell) + " - Стоимость билета");
-        System.out.println((bonus) + " - Количество рублей для одной бонусной милли");
-        System.out.println((sell) + " / " + (bonus) + " = " + (miles) + " - Количество начисляемых бонусных милль");
-
-
+        System.out.println(miles);
     }
 }
